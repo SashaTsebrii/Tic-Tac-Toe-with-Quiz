a0fe90013b42a0627ccd082c212f15be320f2a18
SashaTsebrii <sashatsebrii@gmail.com>
2017-04-12 16:21:22 +0300

    Add some screens in StoryBoard only.

---
+++ Tic-Tac-Toe With Quiz/EnterNameView.swift
@@ -0,0 +1,21 @@
+//
+//  EnterNameView.swift
+//  Tic-Tac-Toe With Quiz
+//
+//  Created by Aleksandr Tsebrii on 4/4/17.
+//  Copyright © 2017 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+class EnterNameView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
